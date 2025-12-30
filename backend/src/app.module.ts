ba927cd7ac92345f545de8573787ed1eb3b9c15a
CauaOdM <CauaSarraf04@gmail.com>
2025-12-30 17:30:44 -0300

    comentarios

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,1 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
+@Module({
+  imports: [
+    // 1. Carrega as variáveis do arquivo .env para a memória
+    ConfigModule.forRoot(),
+
+    // 2. Configura a conexão com o Banco de Dados
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      
+      // AutoLoad: Descobre automaticamente as tabelas (Entidades) que criaremos depois
+      autoLoadEntities: true, 
+      
+      // Sync: Cria as tabelas automaticamente. 
+      // PERIGO: Em produção (empresa real) isso deve ser FALSE, pois pode apagar dados.
+      // Para nosso projeto MVP, deixamos TRUE para ganhar velocidade.
+      synchronize: true, 
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
